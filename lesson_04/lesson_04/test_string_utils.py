54ebe6bdbcfe36ca6474b361fdbe8b19223d57c3
garyaevaon <garyaevaon@gmail.com>
2024-10-25 20:28:20 +0700

    lesson_04

--- lesson_04/lesson_04/test_string_utils.py
+++ lesson_04/lesson_04/test_string_utils.py
@@ -28,20 +28,24 @@ def test_trim_positive_and_negative(input_text, expected_output):
 @pytest.mark.parametrize(
     'string, delimeter, result',
     [
-        ("1:2:3", ":", "1, 2, 3, 4"), #позитивная
-        ("", "", "") #негативная
+        ("1:2:3", ":", ['1', '2', '3']), #позитивная
+        ("№|?,11/#", "/", ['№|?,11', '#']), #позитивная
+        ("   ", "", []), #негативная
+        (None, "", []) #негативная
     ],
 )
 def test_to_list_positive_and_negative(string, delimeter, result):
-    res = processor.to_list(string, delimeter)
-    assert res == result
+        res = processor.to_list(string, delimeter)
+        assert res == result
 
 
 @pytest.mark.parametrize(
     'string, symbol, result',
     [
         ("Текст", "с", True), #позитивная
-        ("Текст", "о", False) #негативная
+        ("Текст", "о", False), #негативная
+        ("  ", "о", False), #негативная
+        ("Текст", "5", False), #негативная
     ],
 )
 def test_contains_positive_and_negative(string, symbol, result):
@@ -54,7 +58,7 @@ def test_contains_positive_and_negative(string, symbol, result):
     [
         ("Текст", "Т", "екст"), #позитивная
         ("Текст", "Тек", "ст"), #позитивная
-        ("Текст", "Р", "Текст") #негативная
+        ("Текст", "#", "Текст") #негативная
     ],
 )
 def test_delete_symbol_positive_and_negative(string, symbol, result):
@@ -77,8 +81,8 @@ def test_starts_with_positive_and_negative(string, symbol, result):
 @pytest.mark.parametrize(
     'string, symbol, result',
     [
-        ("Текст", "Т", True), #позитивная
-        ("Текст", "Р", False) #негативная
+        ("Текст", "т", True), #позитивная
+        ("Текст", "р", False) #негативная
     ],
 )
 def test_end_with_positive_and_negative(string, symbol, result):
@@ -102,9 +106,9 @@ def test_is_empty_positive_and_negative(string, result):
 @pytest.mark.parametrize(
     'lst, joiner, result',
     [
-        ("1,2,3,4", "", "1, 2, 3, 4"), #позитивная
-        ("Sky,Pro", "", "Sky, Pro"), #позитивная
-        ("Sky, Pro", "-", "Sky-Pro") #позитивная
+        ([1,2,3,4], "", "1, 2, 3, 4"), #позитивная
+        (['Sky','Pro'], "", "Sky, Pro"), #позитивная
+        (['Sky','Pro'], "-", "Sky-Pro") #позитивная
     ],
 )
 def test_list_to_string_positive_and_negative(lst, joiner, result):
